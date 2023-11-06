599e0dd3bdc3c9f082a4a1b199e9e8fb4f54fb21
AuroraRyunix <130221060+AuroraRyunix@users.noreply.github.com>
2023-11-06 23:07:57 +0100

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -7,16 +7,32 @@ install_java_debian() {
     sudo apt-get install openjdk-19-jre -y
 }
 
-# Function to install Java on CentOS/RHEL
-install_java_redhat() {
-    echo "CentOS/RHEL based system detected. Installing OpenJDK 19..."
-    sudo yum install jdk-21 -y
-}
-
 # Function to install Java on Fedora
 install_java_fedora() {
-    echo "Fedora based system detected. Installing OpenJDK 19..."
-    sudo dnf install jdk-21 -y
+    echo "Fedora-based system detected. Downloading and installing Oracle JDK 21..."
+    # Download Oracle JDK 21 RPM
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-aarch64_bin.rpm
+
+    # Install Oracle JDK 21
+    sudo rpm -i jdk-21_linux-aarch64_bin.rpm
+
+    # Set Oracle JDK as the default
+    sudo alternatives --set java /usr/java/jdk-21/bin/java
+    sudo alternatives --set javac /usr/java/jdk-21/bin/javac
+}
+
+# Function to install Java on Red Hat/CentOS
+install_java_redhat() {
+    echo "Red Hat/CentOS based system detected. Downloading and installing Oracle JDK 21..."
+    # Download Oracle JDK 21 RPM
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-aarch64_bin.rpm
+
+    # Install Oracle JDK 21
+    sudo rpm -i jdk-21_linux-aarch64_bin.rpm
+
+    # Set Oracle JDK as the default
+    sudo alternatives --set java /usr/java/jdk-21/bin/java
+    sudo alternatives --set javac /usr/java/jdk-21/bin/javac
 }
 
 # Function to install Java on Arch Linux
@@ -31,14 +47,14 @@ install_java() {
         echo "Java is already installed."
     elif [ -f /etc/debian_version ]; then
         install_java_debian
-    elif [ -f /etc/redhat-release ]; then
-        install_java_redhat
     elif [ -f /etc/fedora-release ]; then
         install_java_fedora
+    elif [ -f /etc/redhat-release ]; then
+        install_java_redhat
     elif [ -f /etc/arch-release ]; then
         install_java_arch
     else
-        echo "Unsupported system. Please install OpenJDK 19 manually."
+        echo "Unsupported system. Please install the appropriate JDK manually."
         exit 1
     fi
 }
