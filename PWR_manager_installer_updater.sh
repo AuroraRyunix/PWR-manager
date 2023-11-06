add81cb524b167d2002ecb2bb58f0d6b66148e6e
AuroraRyunix <130221060+AuroraRyunix@users.noreply.github.com>
2023-11-06 19:11:09 +0100

    Update PWR_manager_installer_updater.sh

--- PWR_manager_installer_updater.sh
+++ PWR_manager_installer_updater.sh
@@ -1,39 +1,10 @@
 #!/bin/bash
 
-# Function to install Java on Debian/Ubuntu
-install_java_debian() {
-    sudo apt-get update
-    sudo apt-get install -y default-jre
-}
-
-# Function to install Java on RHEL/CentOS/Fedora
-install_java_redhat() {
-    sudo yum install -y java-1.8.0-openjdk
-}
-
-# Function to install Java on Arch Linux
-install_java_arch() {
-    sudo pacman -S --noconfirm jre8-openjdk
-}
-
-# Function to determine the Linux distribution and install Java
-install_java() {
-    if [ -x "$(command -v java)" ]; then
-        echo "Java is already installed."
-    elif [ -x "$(command -v apt-get)" ]; then
-        install_java_debian
-    elif [ -x "$(command -v yum)" ]; then
-        install_java_redhat
-    elif [ -x "$(command -v pacman)" ]; then
-        install_java_arch
-    else
-        echo "Unsupported Linux distribution. Please install Java manually."
-        exit 1
-    fi
-}
-
-# Check and install Java
-install_java
+# Check if Java is installed
+if ! command -v java &>/dev/null; then
+    echo "Java is not installed. Installing Java..."
+    ./install_java.sh
+fi
 
 # Continue with the rest of the script as before
 # Define the URL of the .jar file to download
@@ -54,7 +25,7 @@ home_dir="$HOME"
 # Create a directory to store all the related files
 mkdir -p "$home_dir/PWR_manager"
 
-# Download the .jar file and store it in the user's home directory
+# Download the ..jar file and store it in the user's home directory
 wget -O "$home_dir/PWR_manager/validator.jar" "$jar_url"
 
 # Create a passwords file (You can customize this as needed)
@@ -84,4 +55,3 @@ systemctl enable $service_name
 systemctl start $service_name
 
 echo "PWR Manager service has been installed and started with auto-restart on failure."
-
