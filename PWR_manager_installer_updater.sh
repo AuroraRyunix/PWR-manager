d5d1c0458393e6888421f9c81ebdc1042bc24a61
AuroraRyunix <130221060+AuroraRyunix@users.noreply.github.com>
2023-11-08 22:10:17 +0100

    Update PWR_manager_installer_updater.sh

--- PWR_manager_installer_updater.sh
+++ PWR_manager_installer_updater.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #this is the main installer script that downloads the required updates, java, PWR node validator executable (jar). After this it creates a system.d service that will manage the service. 
-# Preset locations:
-
+# user changable settings:
+#
+#
+#
+#
+#
+#
+#
+#
 # Define the URL of the .jar file to download
 jar_url="https://github.com/pwrlabs/PWR-Validator-Node/raw/main/validator.jar"
 # Define the service name
@@ -26,7 +33,111 @@ fi
 
 
 
+
+
+
+
+#installing java
+#
+#
+#
+#
+#
+#
+#
+#
+
+install_java_debian() {
+    echo "Debian based system detected. Installing OpenJDK 21..."
+    sudo apt-get update -y
+    sudo apt-get install -y
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+    sudo apt-get -qqy install ./jdk-21_linux-x64_bin.deb
+}
+
+install_java_ubuntu() {
+    echo "Ubuntu based system detected. Installing OpenJDK 21..."
+    sudo apt-get update -y
+    sudo apt-get install -y
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+    sudo apt-get -qqy install ./jdk-21_linux-x64_bin.deb
+}
+
+
+# Function to install Java on Fedora
+install_java_fedora() {
+    echo "Fedora-based system detected. Downloading and installing Oracle JDK 21..."
+    # Download Oracle JDK 21 RPM
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.rpm
+
+    # Install Oracle JDK 21
+    sudo rpm -i jdk-21_linux-x64_bin.rpm
+}
+
+# Function to install Java on Red Hat/CentOS
+install_java_redhat() {
+    echo "Red Hat based system detected. Downloading and installing Oracle JDK 21..."
+    # Download Oracle JDK 21 RPM
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.rpm
+
+    # Install Oracle JDK 21
+    sudo rpm -i jdk-21_linux-x64_bin.rpm
+}
+
+install_java_centos() {
+    echo "CentOS based system detected. Downloading and installing Oracle JDK 21..."
+    # Download Oracle JDK 21 RPM
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.rpm
+
+    # Install Oracle JDK 21
+    sudo rpm -i jdk-21_linux-x64_bin.rpm
+}
+
+# Function to install Java on Arch Linux
+install_java_arch() {
+    echo "Arch Linux detected. Installing OpenJDK 19..."
+    sudo pacman -S wget jre-openjdk --noconfirm
+}
+
+# Function to determine the Linux distribution and install Java
+install_java() {
+    if [ -x "$(command -v java)" ]; then
+        echo "Java is already installed."
+    elif [ -f /etc/debian_version ]; then
+        install_java_debian
+    elif [ -f /etc/fedora-release ]; then
+        install_java_fedora
+    elif [ -f /etc/redhat-release ]; then
+        install_java_redhat
+    elif [ -f /etc/arch-release ]; then
+        install_java_arch
+    elif [ -f /etc/centos-release ]; then
+        install_java_centos
+    elif [ -f /etc/ubuntu-release ]; then
+        install_java_ubuntu
+    else
+        echo "Unsupported system. Please install the appropriate JDK manually."
+        exit 1
+    fi
+}
+
+
+
+
+
+
+
+#
+#
+#
+#
+#
+#
+#
+#
 # Function to perform the uninstallation
+
+
 perform_uninstall() {
     # Add your uninstallation code here
     echo "Uninstalling PWR manager..."
@@ -98,6 +209,16 @@ perform_uninstall() {
 
 }
 
+
+
+#check for --uninstall
+#
+#
+#
+#
+#
+#
+#
 if [ "$1" = "--uninstall" ]; then
     # If the first command-line argument is "--uninstall", perform uninstallation
     perform_uninstall
@@ -108,8 +229,16 @@ else
     # Check if Java is installed
 	if ! command -v java &>/dev/null; then
     		echo "Java is not installed. Installing Java..."
-    		sudo sh install_java.sh
+    		install_java
 	fi
+	
+	
+
+	
+	
+	
+
+	
     mkdir -p "$output_dir"
     sudo chmod 0777 "$output_dir"
     touch "$output_dir/out.txt"
