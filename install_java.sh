b8ecae1b36a658c1873a71f3ce7c84b7c7bdeb5e
AuroraRyunix <130221060+AuroraRyunix@users.noreply.github.com>
2023-11-08 21:37:38 +0100

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -2,13 +2,21 @@
 
 # Function to install Java on Debian/Ubuntu
 install_java_debian() {
-    echo "Debian/Ubuntu based system detected. Installing OpenJDK 19..."
+    echo "Debian based system detected. Installing OpenJDK 21..."
     sudo apt-get update -y
     sudo apt-get install -y
-    wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.deb
-    sudo apt-get -qqy install ./jdk-19_linux-x64_bin.deb
-
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+    sudo apt-get -qqy install ./jdk-21_linux-x64_bin.deb
 }
+
+install_java_ubuntu() {
+    echo "Ubuntu based system detected. Installing OpenJDK 21..."
+    sudo apt-get update -y
+    sudo apt-get install -y
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+    sudo apt-get -qqy install ./jdk-21_linux-x64_bin.deb
+}
+
 
 # Function to install Java on Fedora
 install_java_fedora() {
@@ -22,7 +30,16 @@ install_java_fedora() {
 
 # Function to install Java on Red Hat/CentOS
 install_java_redhat() {
-    echo "Red Hat/CentOS based system detected. Downloading and installing Oracle JDK 21..."
+    echo "Red Hat based system detected. Downloading and installing Oracle JDK 21..."
+    # Download Oracle JDK 21 RPM
+    wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.rpm
+
+    # Install Oracle JDK 21
+    sudo rpm -i jdk-21_linux-x64_bin.rpm
+}
+
+install_java_centos() {
+    echo "CentOS based system detected. Downloading and installing Oracle JDK 21..."
     # Download Oracle JDK 21 RPM
     wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.rpm
 
@@ -48,11 +65,16 @@ install_java() {
         install_java_redhat
     elif [ -f /etc/arch-release ]; then
         install_java_arch
+    elif [ -f /etc/centos-release ]; then
+        install_java_centos
+    elif [ -f /etc/ubuntu-release ]; then
+        install_java_ubuntu
     else
         echo "Unsupported system. Please install the appropriate JDK manually."
         exit 1
     fi
 }
+
 
 # Check and install Java
 install_java
